a15c57c1f7e763845a84b0cebff5d269fe140130
= <=>
2026-03-09 18:08:58 +0530

    setup shadcn

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-7xl">Hello</h1>
+      <Button>CLick Me</Button>
+    </div>
+  );
+}
