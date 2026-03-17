446c123e484b3a238dd4b90c92e7ec6b97d1536b
= <=>
2026-03-17 12:22:27 +0530

    add unique to userId col address table

---
+++ prisma/migrations/20260317065119_add_unique_to_user_id_address_table/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[userId]` on the table `Address` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `updatedAt` to the `Address` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Address" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Address_userId_key" ON "Address"("userId");
